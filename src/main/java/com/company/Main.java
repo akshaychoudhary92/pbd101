1514806684ccfcd52710725f3b43c18025532b2e
Akshay Choudhary <ak92choudhary@gmail.com>
2020-05-02 23:44:15 -0400

    done

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Month m = new Month();
+        m.monthName(1);
+        m.monthName(2);
+        m.monthName(3);
+        m.monthName(4);
+        m.monthName(5);
+        m.monthName(6);
+        m.monthName(7);
+        m.monthName(8);
+        m.monthName(9);
+        m.monthName(10);
+        m.monthName(11);
+        m.monthName(12);
+
+    }
+}
